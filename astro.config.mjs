f68fe3efb1c66d9416dba4e2d64e54373272a53d
Zachary Stout <stout.zachary@gmail.com>
2023-07-14 12:49:20 -0600

    image compression, increased lighthouse score

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.friendlyuser.github.io',
   base: "/",
-  integrations: [tailwind(), react(),],
+  integrations: [tailwind(), react(), image()],
   output: "server",
   adapter: vercel()
 });
