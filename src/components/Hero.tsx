75154954fe97a448384253ca67455a34f503f99f
Zachary Stout <stout.zachary@gmail.com>
2023-08-05 21:59:34 -0600

    added in mobile styles and fixed up some stuff

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,36 +7,31 @@ const Hero = () => {
       className="bg-cover bg-center flex items-center"
       style={{ backgroundImage: `url('./road.jpg')` }}
     >
-      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center text-white ">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center text-white h-screen ">
         <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
           <img
             src="./social_me_icon_lg.png"
             className="h-36 w-auto my-5 rounded-xl"
             alt="Social Me Icon"
           />
-          <h1 className="font-banschrift font-extrabold title-font sm:text-4xl text-3xl mb-4 text-white">
+          <p className="font-source-sans mb-8 leading-relaxed font-extrabold">
             We Bring Therapy To Your Doorstep
-          </h1>
-          <p className="font-source-sans mb-8 leading-relaxed">
+          </p>
+          <p className="font-source-sans mb-8 font-light lg:w-1/2 leading-relaxed">
             Social Me Mobile Therapy uses renovated
             buses-turned-mobile-therapy-offices so that you can get{" "}
             <span className="font-extrabold">
               quality mental health treatment wherever you need it.
             </span>
           </p>
-          <p className="font-source-sans mb-8 leading-relaxed">
+          {/* <p className="font-source-sans mb-8 leading-relaxed">
             Our offices are housed in custom-built buses so that your therapist
             can truly meet you where you're at.
-          </p>
-          <h1 className="font-banschrift title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+          </p> */}
+          {/* <h1 className="font-banschrift title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Social Me Mobile Therapy Can Help
-          </h1>
-          <p className="font-source-sans mb-8 leading-relaxed">
-            - New moms who need to stay close to home <br /> - Individuals who
-            struggle to leave the house due to their severe anxiety or
-            obsessive-compulsive spectrum disorder <br /> - Families with young
-            children <br /> - Members of rural communities <br /> -and more!
-          </p>
+          </h1> */}
+
           <Link to="Contact" smooth={true} duration={500}>
             <button
               type="submit"
--- src/components/WhatWeDo.tsx
+++ src/components/WhatWeDo.tsx
@@ -4,7 +4,7 @@ const WhatWeDo = () => {
   return (
     <section className="bg-gray-100">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-        <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left items-center text-center">
           <h1 className="font-banschrift title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
             We Bring Therapy To You
           </h1>
@@ -21,11 +21,92 @@ const WhatWeDo = () => {
             Social Me Mobile Therapy Can Help
           </h1>
           <p className="font-source-sans mb-8 leading-relaxed">
-            - New moms who need to stay close to home <br /> - Individuals who
-            struggle to leave the house due to their severe anxiety or
-            obsessive-compulsive spectrum disorder <br /> - Families with young
-            children <br /> - Members of rural communities <br /> - ... and
-            more!
+            <div className="flex pb-5">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="#F78100"
+                className="w-6 h-6 mr-3 "
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+              New moms who need to stay close to home
+            </div>
+            <div className="flex pb-5">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="#F78100"
+                className="w-6 h-6 mr-3 flex-shrink-0 "
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+              Individuals who struggle to leave the house due to their severe
+              anxiety or obsessive-compulsive spectrum disorder
+            </div>
+            <div className="flex pb-5">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="#F78100"
+                className="w-6 h-6 mr-3 "
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+              Families with young children
+            </div>
+            <div className="flex pb-5">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="#F78100"
+                className="w-6 h-6 mr-3 "
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+              Members of rural communities
+            </div>
+            <div className="flex pb-5">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="#F78100"
+                className="w-6 h-6 mr-3 "
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+              and more!
+            </div>
           </p>
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
