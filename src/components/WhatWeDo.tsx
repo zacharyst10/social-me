1ba8ae5f2dd5c66806fe8b19df64a0460e613522
Zachary Stout <stout.zachary@gmail.com>
2023-08-11 09:57:19 -0600

    correct pictures

--- src/components/WhatWeDo.tsx
+++ src/components/WhatWeDo.tsx
@@ -8,7 +8,7 @@ const WhatWeDo = () => {
           <img
             className="object-cover object-center rounded"
             alt="hero"
-            src="./slot-canyon.jpg"
+            src="./arches.jpg"
           />
         </div>
         <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center order-2 md:order-2">
