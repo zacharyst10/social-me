caaa3bde44ff807402afcb8058616e8c2c06b72e
Zachary Stout <stout.zachary@gmail.com>
2023-08-31 19:17:26 -0600

    upgraded to astro 3.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,13 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 
-import image from "@astrojs/image";
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.someslc.com',
   base: "/",
-  integrations: [tailwind(), react(), image()],
+  integrations: [tailwind(), react(), ],
   output: "server",
   adapter: vercel()
 });
